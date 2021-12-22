f513ca1ca88a34e372153da7ed7f565e7ae8efc5
Gianni Testa <gianni.testaabelar@mercadolibre.com>
2021-12-22 15:11:37 -0300

    arreglos vistos en consulta

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Usuarios struct {
+	ID            int64   `json:"id"`
+	Nombre        string  `json:"nombre"`
+	Apellido      string  `json:"apellido"`
+	Email         string  `json:"email"`
+	Edad          int64   `json:"edad"`
+	Altura        float32 `json:"altura"`
+	Activo        bool    `json:"activo"`
+	FechaCreacion string  `json:"fecha-creacion"`
+}
 
 func main() {
-	fmt.Println("")
+
+	// Crea un router con gin
+	router := gin.Default()
+	// // Captura la solicitud GET
+	router.GET("/holanombre", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hola Gianni",
+		})
+	})
+
+	// Captura la solicitud GETALL
+	router.GET("/Productos/GetAll", func(c *gin.Context) {
+		usuarios := []Usuarios{}
+		jsonData, _ := os.ReadFile("./usuarios.json")
+		if err := json.Unmarshal([]byte(jsonData), &usuarios); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(200, usuarios)
+	})
+
+	// Corremos nuestro servidor sobre el puerto 8080
+	router.Run()
 }
